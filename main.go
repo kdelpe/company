361e45381ef3b744cf6ad8042fcdaa92594a39cf
Kervens Delpe <kdelpe@gmail.com>
2024-03-10 22:56:34 -0400

    Server basic setup

--- main.go
+++ main.go
@@ -2,8 +2,24 @@ package main
 
 import (
 	"example/company/mysql"
+	"example/company/server"
+	"github.com/gin-gonic/gin"
+	"log"
 )
 
 func main() {
 	mysql.Connection()
+
+	router := gin.Default()
+
+	router.GET("/branches", server.GETBranches)
+	router.GET("/branches/:id", server.GETBranch)
+	router.GET("/employees", server.GETEmployees)
+	router.GET("/employees/:id", server.GETEmployee)
+
+	//Start server
+	err := router.Run("localhost:9090")
+	if err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
 }
---
+++ server/employeeHandler.go
@@ -0,0 +1,126 @@
+package server
+
+import (
+	"errors"
+	"example/company/mysql"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+)
+
+func GETEmployees(c *gin.Context) {
+	db := mysql.RetrieveDatabase()
+
+	query := `
+	        SELECT 
+            e.emp_id, 
+            e.first_name, 
+            e.last_name, 
+            e.birth_date, 
+            e.sex, 
+            e.salary, 
+            e.super_id, 
+            b.branch_id, 
+            b.branch_name, 
+            b.mgr_id, 
+            b.mgr_start_date 
+        FROM 
+            Employee e
+        LEFT JOIN 
+            Branch b ON e.branch_id = b.branch_id
+        LEFT JOIN 
+            Employee mgr ON b.mgr_id = mgr.emp_id
+        LEFT JOIN 
+            Employee super ON e.super_id = super.emp_id;
+`
+
+	rows, err := db.Query(query)
+	if err != nil {
+		log.Println("Error querying employees: ", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
+		return
+	}
+
+	var employees []Employee
+	for rows.Next() {
+		var employee Employee
+		var branch Branch
+		if err := rows.Scan(&employee.EmpID, &employee.FirstName, &employee.LastName, &employee.BirthDate, &employee.Sex,
+			&employee.Salary, &employee.SuperID, &branch.BranchID, &branch.BranchName,
+			&branch.MgrID, &branch.MgrStartDate); err != nil {
+			log.Println("Error retrieving employees", err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
+			return
+		}
+
+		employee.Branch = branch
+
+		employees = append(employees, employee)
+	}
+	c.IndentedJSON(http.StatusOK, employees)
+}
+
+func GETEmployee(c *gin.Context) {
+	db := mysql.RetrieveDatabase()
+
+	empID := c.Param("id")
+
+	row := db.QueryRow("SELECT * FROM employee WHERE emp_id = ?", empID)
+
+	var employee Employee
+	if err := row.Scan(&employee.EmpID, &employee.FirstName, &employee.LastName, &employee.BirthDate, &employee.Sex, &employee.Salary, &employee.SuperID, &employee.Branch.BranchID); err != nil {
+		log.Println("No Employee Found", err)
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Bad Request"})
+		return
+	}
+	c.IndentedJSON(http.StatusOK, employee)
+}
+
+//	func POSTEmployee(c *gin.Context) {
+//		db := mysql.RetrieveDatabase()
+//
+//		var employee Employee
+//		if err := c.ShouldBindJSON(&employee); err != nil {
+//			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+//			return
+//		}
+//
+//		row, err := db.Exec("INSERT INTO employee VALUES(?, ?, ?, ?, ?, ?, ?, ?)", employee.EmpID,
+//			employee.FirstName, employee.LastName, employee.BirthDate, employee.Sex, employee.Salary, employee.SuperID, employee.Branch.BranchID)
+//		if err != nil {
+//			log.Println("Error inserting a new employee", err)
+//			return
+//		}
+//
+//		empID, err := row.LastInsertId()
+//		if err != nil {
+//			log.Println("Error retrieving new employee ID", err)
+//			return
+//		}
+//
+//		employee.EmpID = empID
+//		c.IndentedJSON(http.StatusCreated, employee)
+//	}
+func validateEmployeeData(employee Employee) error {
+	if employee.FirstName == "" {
+		return errors.New("first name is required")
+	}
+
+	if employee.LastName == "" {
+		return errors.New("last name is required")
+	}
+
+	if employee.BirthDate.IsZero() {
+		return errors.New("birth date is required")
+	}
+
+	if employee.Salary <= 0 {
+		return errors.New("salary must be greater than zero")
+	}
+
+	if employee.Branch.BranchID <= 0 {
+		return errors.New("employee's branch must be specified")
+	}
+
+	return nil
+}
